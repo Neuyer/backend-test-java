b931dc569c13ecc8d1b66731ce9057c6d787cfbc
Ariel Neumeyer <arielneu@outlook.com>
2019-08-14 11:44:28 -0300

    swagger

---
+++ src/main/java/br/com/estacionamento/estacionamento/model/Veiculo.java
@@ -0,0 +1,63 @@
+package br.com.estacionamento.estacionamento.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+import javax.validation.constraints.NotEmpty;
+
+@Entity
+public class Veiculo {
+	
+	@Id @GeneratedValue(strategy = GenerationType.IDENTITY) 
+	private Long id;
+	
+	@NotEmpty
+	private String marca;
+	@NotEmpty
+	private String modelo;
+	@NotEmpty
+	private String cor;
+	@NotEmpty
+	private String placa;
+	@NotEmpty
+	private String tipo;
+	@ManyToOne
+	private Estabelecimento estacionamento;
+	
+	public Veiculo() {
+		super();
+	}
+	public String getMarca() {
+		return marca;
+	}
+	public void setMarca(String marca) {
+		this.marca = marca;
+	}
+	public String getModelo() {
+		return modelo;
+	}
+	public void setModelo(String modelo) {
+		this.modelo = modelo;
+	}
+	public String getCor() {
+		return cor;
+	}
+	public void setCor(String cor) {
+		this.cor = cor;
+	}
+	public String getPlaca() {
+		return placa;
+	}
+	public void setPlaca(String placa) {
+		this.placa = placa;
+	}
+	public String getTipo() {
+		return tipo;
+	}
+	public void setTipo(String tipo) {
+		this.tipo = tipo;
+	}
+}
+
